595a8ca63a2023a80db7ed994ce16f9891d70fcb
Ahamisi <ahamisivickzyjr@gmail.com>
2020-08-23 23:27:08 +0100

    adding header components and links to home, shop page and contact

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -3,18 +3,20 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
 import  './pages/homepage/homepage.styles.scss';
-import ShopPage from '././pages/shop/shop.component'
-const HatsPage = () =>{
-  return (
-    <div>
-      <h1>Hatss Pages</h1>
-    </div>
-  )
-}
+import ShopPage from '././pages/shop/shop.component';
+import Header from './components/header/header.component';
+// const HatsPage = () =>{
+//   return (
+//     <div>
+//       <h1>Hatss Pages</h1>
+//     </div>
+//   )
+// }
 
 function App() { 
   return (
     <div>
+      <Header />
       <Switch>
         <Route path="/" exact="true" component={HomePage} />
         {/* <Route path="/shop/hats" exact="true" component={HatsPage} /> */}
